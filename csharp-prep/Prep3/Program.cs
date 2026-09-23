ca561e6e6ecea06f1fb72599f0c03bf2e8cf5a89
Camila Ramirez Suarez <ram22042@byui.edu>
2026-09-22 23:04:03 -0600

    p3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        int magicGuess = -1;
+
+        while (magicGuess != magicNumber)
+        {
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        magicGuess = int.Parse(guess);
+
+        if (magicGuess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (magicGuess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        }
+        
     }
 }
